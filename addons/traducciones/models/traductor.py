110577389fcb70f1571264af6dad04844ac0571b
TeresaGarcia <teresagar181@gmail.com>
2019-03-05 18:29:25 +0100

    Añadido campo onchange
    Signed-off-by: TeresaGarcia <teresagar181@gmail.com>

--- addons/traducciones/models/traductor.py
+++ addons/traducciones/models/traductor.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models,fields
-    
+from odoo import models, fields, api
+from datetime import timedelta, date, datetime
+from dateutil.relativedelta import relativedelta
+
+
 class traductor(models.Model):
     _name = 'tradus.traductor'
-    dni =  fields.Char()
+    dni = fields.Char()
     nombre = fields.Char()
     apellidos = fields.Char()
     fecha_incorporacion = fields.Date()
-    id_idiomas = fields.Many2many(string= "idiomas",comodel_name='tradus.idioma',relation = 'rel_traductor_idiomas',column1='traductor', column2='idioma')
+    id_idiomas = fields.Many2many(string="idiomas", comodel_name='tradus.idioma',
+                                  relation='rel_traductor_idiomas', column1='traductor', column2='idioma')
+    tiempo = fields.Integer(compute="_value_pc", store=True)
+
+    @api.onchange('fecha_incorporacion')
+    def _value_pc(self):
+        for r in self:
+            if r.fecha_incorporacion:
+                dt = r.fecha_incorporacion
+                d1 = datetime.strptime(dt, "%Y-%m-%d").date()
+                self.tiempo = relativedelta(date.today(), d1).years
+                return {
+                    'warning': {
+                    'title': "Tiempo",
+                    'message': "Tiempo en la empresa actualizado",
+                    },
+                }
